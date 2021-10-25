ec0be7c2c2f5b03b425da9bcbbf2b50751fb073e
unknown <yudhanjeffri168@gmail.com>
2021-10-25 12:16:36 +0700

    add banner

--- lib/screens/dashboard/CarouselWithDotsPage.dart
+++ lib/screens/dashboard/CarouselWithDotsPage.dart
@@ -28,35 +28,6 @@ class _CarouselWithDotsPageState extends State<CarouselWithDotsPage> {
                       fit: BoxFit.cover,
                       width: 1000,
                     ),
-                    Positioned(
-                      bottom: 0.0,
-                      left: 0.0,
-                      right: 0.0,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color.fromARGB(200, 0, 0, 0),
-                              Color.fromARGB(0, 0, 0, 0),
-                            ],
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                          ),
-                        ),
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 10,
-                        ),
-                        child: Text(
-                          'No. ${widget.imgList.indexOf(item)} image',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
                   ],
                 ),
               ),
--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -10,8 +10,8 @@ class DashboardPage extends StatefulWidget {
 }
 
 final List<String> imgList = [
-  'assets/images/banner.png',
-  'assets/images/splashscreen.png',
+  'https://raw.githubusercontent.com/YudhanJeffri/FinaroFlutter/main/assets/images/banner.png',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
   'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
 ];
 
